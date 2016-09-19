b82ba6660524db1a2a12a82e324ec94baf3f60b7
Anders Wind <awia00@gmail.com>
2016-09-19 23:31:43 +0200

    Finished exercise 3.2

---
+++ Exercises/Exercise week 3/ANSWERS.md
@@ -0,0 +1,13 @@
+# Exercise Week 3
+*Awis: Anders Wind*
+
+## 3.1
+See Example154.java for all the implementations. Note I am missing the first flatMap implementation.
+
+## 3.2 
+See TestCountPrimesParallel.java for the implemenation.
+
+## 3.3
+
+
+## 3.4
---
+++ Exercises/Exercise week 3/TestCountPrimesParallel.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+import java.util.stream.*;
+
+public class TestCountPrimesParallel {
+  public static void main(String[] args) {
+    final int N = 10_000_000;
+    final int[] a = IntStream.rangeClosed(1, N).toArray();
+    Arrays.parallelSetAll(a, (x) -> isPrime(x)?1:0);
+    Arrays.parallelPrefix(a, (x,y) -> x+y);
+    System.out.println(a[N-1]);
+    for(int i = N/10-1; i<N; i+=N/10)
+    {
+      System.out.println(a[i]/(i/ Math.log(i)));
+    }
+  }
+
+  private static boolean isPrime(int n) {
+    int k = 2;
+    while (k * k <= n && n % k != 0)
+      k++;
+    return n >= 2 && k * k > n;
+  }
+}
