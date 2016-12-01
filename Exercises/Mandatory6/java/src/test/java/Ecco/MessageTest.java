c652739c92fc38a09a7da067d61f4d08fe03979c
Anders Wind <awia00@gmail.com>
2016-12-01 13:51:51 +0100

    Tests and something else - just to see how intelliJ works

---
+++ Exercises/Mandatory6/java/src/test/java/Ecco/MessageTest.java
@@ -0,0 +1,19 @@
+package Ecco;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by AndersWindSteffensen on 2016-12-01.
+ */
+public class MessageTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+}
